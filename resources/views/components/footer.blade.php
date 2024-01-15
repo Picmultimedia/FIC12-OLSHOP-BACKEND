f7773e3a9181a58f3207c0fade7871c7caaea45d
Picmultimedia <jufri.conoras@gmail.com>
2024-01-15 15:47:37 +0700

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2024 <div class="bullet"></div> Develop By <a href="https://www.instagram.com/picmultimedia/">Picmultimedia Studio</a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
